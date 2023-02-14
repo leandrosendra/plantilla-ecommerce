ae187f910d4ae44828fea50071978cafb000c431
MatiasCarballo <matiascarballo03@hotmail.com>
2023-02-14 18:41:28 -0300

    a .env

--- api/src/database/db.js
+++ api/src/database/db.js
@@ -1,13 +1,21 @@
 const {Sequelize} =require ('sequelize');
+require('dotenv').config();
+const {
+    NAME_BD,
+    USER_DB,
+    PASS_DB,
+    HOST_DB
+}= process.env
 const productM = require ('../models/Product.js');
 const categoryM = require ('../models/Category.js');
 const cartM = require ('../models/Cart.js');
 const userM = require ('../models/User.js');
+const commentM = require ('../models/Comment.js');
 
 
-const sequelize = new Sequelize('plantillas', 'postgres', 'root', {
-    host: 'localhost',
-    dialect: 'postgres',
+const sequelize = new Sequelize(NAME_BD, USER_DB, PASS_DB, {
+    host: HOST_DB,
+    dialect: 'postgres', 
     logging:false 
 });
 //const bd = new Sequelize (`postgres://postgres:root@localhost:5432/plantillas`,{ logging:false });
@@ -16,8 +24,9 @@ productM(sequelize);
 categoryM(sequelize)
 cartM(sequelize)
 userM(sequelize)
+commentM(sequelize)
 
-const{ Product, Category, Cart, User} = sequelize.models;
+const{ Product, Category, Cart, User, Comment} = sequelize.models;
  
 //console.log(sequelize.models)
 
--- api/src/routes/Product.js
+++ api/src/routes/Product.js
@@ -1,62 +1,67 @@
-const { Router } = require('express');
-const { Product, Category } = require('../database/db.js')
+const { Router } = require("express");
+const { Product, Category } = require("../database/db.js");
 const router = Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const product = await Product.findAll({
-            include:[{
-                model: Category
-            }]
-        })
-        res.status(200).json(product)
-    } catch (err) {
-        console.log(err);
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const product = await Product.findAll({
+      include: [
+        {
+          model: Category,
+        },
+      ],
+    });
+    res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-router.post('/', async (req, res) => {
-    const { name, description, stock, image, price, categoryId } = req.body;
-    try {
-        const product = await Product.create({
-            name, description, stock, image, price
-        })
-        product.setCategory(categoryId)
-        res.status(200).json(product)
-    } catch (err) {
-        console.log(err);
-    }
-})
+router.post("/", async (req, res) => {
+  const { name, description, stock, image, price, categoryId } = req.body;
+  try {
+    const product = await Product.create({
+      name,
+      description,
+      stock,
+      image,
+      price,
+    });
+    product.setCategory(categoryId);
+    res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-router.put('/:id', async (req, res) => {
-    const { id } = req.params;
-    const { name, price, image, stock, description } = req.body;
-    try {
-        const product = await Product.findByPk(id)
-        product.name = name;
-        product.price = price;
-        product.image = image;
-        product.description = description;
-        product.stock = stock;
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, price, image, stock, description } = req.body;
+  try {
+    const product = await Product.findByPk(id);
+    product.name = name;
+    product.price = price;
+    product.image = image;
+    product.description = description;
+    product.stock = stock;
 
-        await product.save()
-        res.status(200).json(product)
-    } catch (err) {
-        console.log(err);
-    }
-})
+    await product.save();
+    res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        const product = await Product.destroy({
-            where: { id: id },
-        });
-        res.status(200).json(product);
-    } catch (err) {
-        console.log(err);
-    }
-})
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.destroy({
+      where: { id: id },
+    });
+    res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-
-module.exports = router
+module.exports = router;
