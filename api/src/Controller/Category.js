2b19c342aa4cf2c1244735113773af88e3119f3e
MatiasCarballo <matiascarballo03@hotmail.com>
2023-02-23 17:51:33 -0300

    Controllers

---
+++ api/src/Controller/Category.js
@@ -0,0 +1,76 @@
+const { Category, Size, Color } = require('../database/db.js');
+
+const getCategory = async(req, res)=>{
+    try{
+        const category = await Category.findAll();
+        res.status(200).json(category);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send('err en get category');
+    }
+}
+
+const getColors = async(req,res)=>{
+    try{
+        const color = await Color.findAll();
+        res.status(200).json(color);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send('err en color');
+    }
+}
+
+const getSize = async(req,res)=>{
+    try{
+        const size = await Size.findAll();
+        res.status(200).json(size);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send('err en get size');
+    }
+}
+
+const postCategory = async(req, res)=>{
+    const hola = req.body;
+    try {
+        const category = await Category.bulkCreate(hola);
+        res.status(200).json(category);
+    } catch (err){
+        console.log(err);
+        res.status(400).send('err en post category');
+    }
+}
+
+const postColor = async(req, res)=>{
+    const names = req.body;
+    try {
+        const color = await Color.bulkCreate(names);
+        res.status(200).json(color);
+    } catch (err){
+        console.log(err);
+        res.status(400).send('err en post Color');
+    }
+}
+
+const postSize = async(req, res)=>{
+    const ids = req.body;
+    try {
+        const size = await Size.bulkCreate(ids);
+        res.status(200).json(size);
+    } catch (err){
+        console.log(err);
+        res.status(400).send('err en post Size');
+    }
+}
+
+module.exports= {
+    getCategory,
+    getColors,
+    getSize,
+    postCategory,
+    postColor,
+    postSize
+}
---
+++ api/src/Controller/User.js
@@ -0,0 +1,53 @@
+const { User } = require('../database/db.js')
+
+const getUser = async (req, res) => {
+    try {
+        const user = await User.findAll()
+        res.status(200).json(user)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const postUser = async (req, res) => {
+    const { name, email, password, isAdmin } = req.body;
+    try {
+        const user = await User.create({
+            name, email, password, isAdmin
+        })
+        res.status(200).json(user)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const bulkUser = async(req,res)=>{
+    const bulk = req.body;
+    console.log(bulk)
+    try {
+        const user = await User.bulkCreate(bulk)
+        res.status(200).json(user) 
+    } catch (err) {
+        console.log(err);
+        console.log('err create user for bulk');
+    }
+}
+
+const deleteUser = async (req, res) => { 
+    const { id } = req.params
+    try {
+        const user = await User.destroy({
+            where: { id: id },
+        });
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = {
+    getUser,
+    postUser,
+    bulkUser,
+    deleteUser
+}
